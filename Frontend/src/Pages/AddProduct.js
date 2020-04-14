a2df48237dd2e399faebeb0e59124f8f92d3a275
Yusuf-Divya-Stylisma-au4 <s.diva8826@gmail.com>
2020-04-14 18:36:31 +0530

    updated

--- Frontend/src/Pages/AddProduct.js
+++ Frontend/src/Pages/AddProduct.js
@@ -13,6 +13,7 @@ import wish from '../photos/wishIcon.png'
 
 import axios, { post } from 'axios';
 
+<<<<<<< Updated upstream
   class AddProduct extends React.Component {
 
     handleProductNameChange = (value) => {
@@ -44,6 +45,55 @@ import axios, { post } from 'axios';
     }
     handleproductdetailsChange = (event) => {
       this.props.dispatch({ type: "productdetails", payload: event.target.value })
+=======
+class AddProduct extends React.Component {
+  handleProductNameChange = (event) => {
+    this.props.dispatch({ type: 'productname', payload: event.target.value });
+  };
+  handleProductImageChange = (event) => {
+    this.props.dispatch({ type: 'productimage', payload: event.target });
+  };
+  handleProductRatingChange = (event) => {
+    this.props.dispatch({ type: 'productrating', payload: event.target.value });
+  };
+  handleProductQuantityChange = (event) => {
+    this.props.dispatch({
+      type: 'productquantity',
+      payload: event.target.value,
+    });
+  };
+  handleProductPriceChange = (event) => {
+    this.props.dispatch({ type: 'productprice', payload: event.target.value });
+  };
+  handleProductColorChange = (event) => {
+    this.props.dispatch({ type: 'productcolor', payload: event.target.value });
+  };
+  handleCategoriesChange = (event) => {
+    this.props.dispatch({ type: 'CATEGORIES', payload: event.target.value });
+  };
+  handleSizeChange = (event) => {
+    this.props.dispatch({ type: 'SIZE', payload: event.target.value });
+  };
+  handleSectionChange = (event) => {
+    this.props.dispatch({ type: 'SECTION', payload: event.target.value });
+  };
+  handleproductdetailsChange = (event) => {
+    this.props.dispatch({
+      type: 'productdetails',
+      payload: event.target.value,
+    });
+  };
+
+  validateAddProductDetails = () => {
+    if (
+      this.props.CATEGORIES !== '' &&
+      this.props.SIZE !== '' &&
+      this.props.SECTION !== ''
+    ) {
+      return true;
+    } else {
+      return false;
+>>>>>>> Stashed changes
     }
 
     validateAddProductDetails = () => {
@@ -75,6 +125,7 @@ import axios, { post } from 'axios';
       type: "addProductDetails"
     })
 
+<<<<<<< Updated upstream
 
         console.log("pushedd..." , this.props.productdetails)
 
@@ -130,6 +181,47 @@ import axios, { post } from 'axios';
           <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
           <a className="navbar-brand" href="/home">
             <img src="bird.jpg" alt="logo" style={{width:"20px"}} />
+=======
+       
+handleProductImageUpload = (event) => {
+  // let image = event.target.files[0].name;
+  const files = event.target.files;
+  const data = new FormData()
+  data.append('file',files[0])
+  data.append('upload_preset','darwin')
+  axios
+    .post(
+      'https://api.cloudinary.com/v1_1/yuspat/image/upload',data)
+    .then((res) => {
+      console.log(res.data);
+    });
+    
+  })
+
+    axios.post('http://localhost:5000/addproduct', productdata).then((res) => {
+      console.log(res.data);
+    });
+
+    this.props.dispatch({
+      type: 'addProductDetails',
+      payload: productdata,
+    });
+
+    console.log('pushedd...', this.props.productdata);
+
+    alert('Your product is added Successfully');
+  };
+
+  render() {
+    // console.log("login" , this.props)
+
+    return (
+      <div style={{ justifyContent: 'start' }}>
+        {/* ---------------------------------Navbar----------------------------------------
+        <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
+          <a className='navbar-brand' href='/home'>
+            <img src='bird.jpg' alt='logo' style={{ width: '20px' }} />
+>>>>>>> Stashed changes
           </a>
             <ul className="navbar-nav">
               <li className="nav-item"><a className="nav-link" href="/">Logo</a></li>
@@ -234,6 +326,7 @@ import axios, { post } from 'axios';
 
               <div className='row'>
 
+<<<<<<< Updated upstream
                 <div className='col'>
                   <h3>CATEGORIES : </h3>
                   <input type="radio" 
@@ -364,6 +457,241 @@ import axios, { post } from 'axios';
                   onChange={(event) => this.handleCategoriesChange(event)}
                   />
                   <label for="Night-Suits">Night-Suits</label>
+=======
+                <br />
+                <br />
+
+                <div className='row'>
+                  <div className='col'>
+                    <h3>CATEGORIES : </h3>
+                    <input
+                      type='radio'
+                      id='Jeans'
+                      name='CATEGORIES'
+                      value='Jeans'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Jeans'>Jeans</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='T-Shirts'
+                      name='CATEGORIES'
+                      value='T-Shirts'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='T-Shirts'>T-Shirts</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Lowers'
+                      name='CATEGORIES'
+                      value='Lowers'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Lowers'>Lowers</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Hoddies'
+                      name='CATEGORIES'
+                      value='Hoddies'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Hoddies'>Hoddies</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Western-Dresses'
+                      name='CATEGORIES'
+                      value='Western-Dresses'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Western-Dresses'>Western-Dresses</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Tradional-Dresses'
+                      name='CATEGORIES'
+                      value='Tradional-Dresses'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Tradional-Dresses'>Tradional-Dresses</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Shoes'
+                      name='CATEGORIES'
+                      value='Shoes'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Shoes'>Shoes</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Tops'
+                      name='CATEGORIES'
+                      value='Tops'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Tops'>Tops</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Crop-Tops'
+                      name='CATEGORIES'
+                      value='Crop-Tops'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Crop-Tops'>Crop-Tops</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Sandals'
+                      name='CATEGORIES'
+                      value='Sandals'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Sandals'>Sandals</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Hand-Bags'
+                      name='CATEGORIES'
+                      value='Hand-Bags'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Hand-Bags'>Hand-Bags</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Jwellery'
+                      name='CATEGORIES'
+                      value='Jwellery'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Jwellery'>Jwellery</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='Night-Suits'
+                      name='CATEGORIES'
+                      value='Night-Suits'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleCategoriesChange(event)}
+                    />
+                    <label for='Night-Suits'>Night-Suits</label>
+                  </div>
+
+                  <div className='col'>
+                    <h3>SIZE : </h3>
+                    <input
+                      type='radio'
+                      id='S'
+                      name='SIZE'
+                      value='S'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSizeChange(event)}
+                    />
+                    <label for='S'>Small</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='M'
+                      name='SIZE'
+                      value='M'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSizeChange(event)}
+                    />
+                    <label for='M'>Medium</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='L'
+                      name='SIZE'
+                      value='L'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSizeChange(event)}
+                    />
+                    <label for='L'>Large</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='XL'
+                      name='SIZE'
+                      value='XL'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSizeChange(event)}
+                    />
+                    <label for='XL'>Extra Large</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='XXL'
+                      name='SIZE'
+                      value='XXL'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSizeChange(event)}
+                    />
+                    <label for='XXL'>Double Extra Large</label>
+                  </div>
+
+                  <div className='col'>
+                    <h3>SECTION : </h3>
+                    <input
+                      type='radio'
+                      id='men'
+                      name='SECTION'
+                      value='men'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSectionChange(event)}
+                    />
+                    <label for='men'>Men</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='women'
+                      name='SECTION'
+                      value='women'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSectionChange(event)}
+                    />
+                    <label for='women'>Women</label>
+                    <br />
+                    <input
+                      type='radio'
+                      id='kids'
+                      name='SECTION'
+                      value='kids'
+                      // value={this.props.productname}
+                      onChange={(event) => this.handleSectionChange(event)}
+                    />
+                    <label for='kids'>Kids</label>
+                  </div>
+
+                  <div className='col'>
+                    <button
+                      id='addproduct'
+                      className='btn btn-success'
+                      value='upload'
+                      onClick={() => {this.handleAddProductButton();this.handleProductImageUpload()}}
+                      disabled={!this.validateAddProductDetails()}>
+                      ADD PRODUCT
+                    </button>
+                  </div>
+>>>>>>> Stashed changes
                 </div>
 
                 
