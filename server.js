19879c24f427abb05d8c0328f7d79e01cac4ce56
Yusuf Pathan <yusufp04@gmail.com>
2020-05-03 13:26:08 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -5,15 +5,6 @@ const logger = require('morgan');
 const API_PORT = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "./Frontend/public"));
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, './Frontend/public/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
-
-
 
 //middlewares
 app.use(express.json());
