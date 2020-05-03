c9ea7fc4b4de8cdb70151247216bfdb2e793ebf0
Yusuf Pathan <yusufp04@gmail.com>
2020-05-03 13:28:49 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const logger = require('morgan');
 const API_PORT = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "./Frontend/public"));
+app.use(express.static(__dirname + "./Frontend/public/index.html"));
 
 //middlewares
 app.use(express.json());
